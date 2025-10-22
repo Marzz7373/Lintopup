63a1d06376b17d2a007d86c86baa02cd92a95c0f
Marzz7373 <mnmarzuqi@gmail.com>
2025-10-22 19:29:20 +0800

    Update and rename script.js to styles.css

--- script.js
+++
@@ -1,6 +0,0 @@
-// Terminal functionality
-const terminalInput = document.getElementById('terminalInput');
-const terminalOutput = document.getElementById('terminalOutput');
-
-// Command database
-const commands = {
