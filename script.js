ea614cf331f255ba6fd960ff3156e725d5c9e99e
Marzz7373 <mnmarzuqi@gmail.com>
2025-10-22 19:26:04 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Terminal functionality
+const terminalInput = document.getElementById('terminalInput');
+const terminalOutput = document.getElementById('terminalOutput');
+
+// Command database
+const commands = {
